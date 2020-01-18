d7de909215df86833003e01d584cf23d10dc7281
Bilal Ali Syed <sayedbilalali614@gmail.com>
2020-01-18 20:32:18 +0530

    Add project files.

---
+++ ScheduleTasks/EmailJob.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using Quartz;
+using System.Threading.Tasks;
+
+namespace ScheduledTaskExample.ScheduleTasks
+{
+    public class EmailJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("On Executing the Mail --->");
+            using (var message = new MailMessage("syedbilalali.dev@gmail.com", "praveen@younggeeks.in"))
+            {
+                message.Subject = "Test";
+                message.Body = "Test at " + DateTime.Now;
+                using (SmtpClient client = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential("syedbilalali.dev@gmail.com", "syedbilal999")
+                })
+               
+                    client.Send(message);
+                
+            }
+            System.Diagnostics.Debug.WriteLine("On Sent the Mail --->");
+        }
+    }
+}
---
+++ ScheduleTasks/JobScheduler.cs
@@ -0,0 +1,32 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Quartz;
+using Quartz.Impl;
+
+namespace ScheduledTaskExample.ScheduleTasks
+{
+    public class JobScheduler
+    {
+        public static void Start()
+        {
+            IScheduler scheduler = (IScheduler)StdSchedulerFactory.GetDefaultScheduler();
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<EmailJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+             .WithIdentity("trigger1", "group1")
+    .StartNow()
+    .WithSimpleSchedule(x => x
+        .WithIntervalInSeconds(10)
+        .RepeatForever())
+                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
